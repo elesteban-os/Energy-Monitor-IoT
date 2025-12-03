4402b7e8d29c3f75606680d688fb31b4d1eeeba1
yonathan_ms146 <yonathan0146@estudiantec.cr>
2025-12-02 18:30:00 -0600

    commit-merge

--- backend/seed_mongo.py
+++ backend/seed_mongo.py
@@ -5,7 +5,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 # -----------------------------
-# Configuración de MongoDB Atlas
+# Configuración de MongoDB Atlass
 # -----------------------------
 
 
